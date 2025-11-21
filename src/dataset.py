bbdb8cf430165ee01c2361c1deef0a348345f72c
El0wki <92699365+El0wki@users.noreply.github.com>
2025-11-21 17:53:16 -0300

    Organizando as chamadas de função

--- src/dataset.py
+++ src/dataset.py
@@ -1,24 +1,69 @@
-def extractDataSet():
-    import kagglehub
-    from kagglehub import KaggleDatasetAdapter
+class DataSet:
+    def __init__(self, datasetName, path):
+        self.df = None
+        self.name = datasetName
+        self.path = path
+        self.extractDataSet()
 
-    path = "Titanic-Dataset.csv"
+    def extractDataSet(self):
+        import kagglehub
+        from kagglehub import KaggleDatasetAdapter
+        try:
+            self.df = kagglehub.dataset_load(
+                KaggleDatasetAdapter.PANDAS,
+                self.name,
+                self.path
+            )   
+        except Exception as e:
+            print("Erro ao importar dados")
+        
+    def sampleData(self, sampleNum=5):
+        try:
+            print("shape:", self.df.shape)
+            print("colunas:", self.df.columns)
+            print("tipos:",self.df.dtypes)
+            print("amostras:",self.df.sample(sampleNum))
+        except Exception as e:
+            print("Erro ao amostrar dados")
+        
 
-    df = kagglehub.dataset_load(
-    KaggleDatasetAdapter.PANDAS,
-    "yasserh/titanic-dataset",
-    path
-    )   
-    return df
+    def cleanseData(self):
+        try:
+            print("Valores nulos por coluna:")
+            nulls = self.df.isnull().sum()
+            print(nulls[nulls > 0])
 
-def startingSample(df, sampleNum=5):
-    print(df.shape)
-    print(df.columns)
-    print(df.dtypes)
-    print(df.sample(sampleNum))
+            for col in self.df.columns:
+                if self.df[col].isnull().any():
+                    if self.df[col].dtype in ['float64', 'int64']:
+                        self.df[col] = self.df[col].fillna(self.df[col].mean())
+                    else:
+                        self.df[col] = self.df[col].fillna(self.df[col].mode()[0])
 
-def cleanseData(df):
-
-
-def filterDataBy(df):
+        except Exception as e:
+            print("Erro ao limpar dados")
+    
+    def filterData(self):
+        try:
+            print("sla")
+        except Exception as e:
+            print("Erro ao filtrar dados")
+    
+    def createNewColumn(self):
+        try:
+            print("sla")
+        except Exception as e:
+            print("Erro ao criar nova coluna")
+    
+    def groupBy(self):
+        try:
+            print("sla")
+        except Exception as e:
+            print("Erro ao agrupar dados")
+    
+    def graph(self):
+        try:
+            print("sla")
+        except Exception as e:
+            print("Erro ao filtrar dados")
 
--- src/main.py
+++ src/main.py
@@ -1,46 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from src.dataset import *
+from dataset import DataSet
 
-df = extractDataSet()
-startingSample(df)
-cleanseData(df)
-filterData(df)
-#informacoes e tipos de dados
-df.info()
+datasetName = "yasserh/titanic-dataset"
+datasetPath = "Titanic-Dataset.csv"
 
-#primeiras linhas
-df.head(5)
-
-#Homens e mulheres
-homens = df[ (df["Sex"] == "male")  ].count().sum()
-mulheres = df[ (df["Sex"] == "female")  ].count().sum()
-print("Homens::", homens)
-print("Mulheres:", mulheres)
-
-valores = [homens, mulheres]
-categorias = ['Homens', 'Mulheres']
-
-plt.bar(categorias, valores, color='skyblue')
-plt.show()
-
-#Mortos e sobreviventes
-mortos = df[ (df['Survived'] == 0)  ].count().sum()
-sobreviventes = df[ (df['Survived'] == 1)  ].count().sum()
-
-print("Mortos:", mortos)
-print("Sobreviventes:", sobreviventes)
-
-valores = [mortos, sobreviventes]
-categorias = ['Mortos', 'Sobreviventes']
-
-plt.bar(categorias, valores, color='skyblue')
-plt.show()
-
-#Idade
-idades = df["Age"]
-plt.hist(idades)
-plt.show()
-
-#Mortos e sobreviventes x idade
-print(idades, valores)
+dataset = DataSet(datasetName, datasetPath)
+dataset.sampleData()
+dataset.cleanseData()
